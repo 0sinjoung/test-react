6af91443830c804772965e8f7d6112f737167d3b
jyst <yeongsinjoung@gmail.com>
2021-10-10 15:07:49 +0900

    update

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import React, {Component} from "react";
 import TOC from "./components/TOC"
 import ReadContent from "./components/ReadContent"
 import CreateContent from "./components/CreateContent"
+import UpdateContent from "./components/UpdateContent"
 import Subject from "./components/Subject"
 import Control from "./components/Control"
 
@@ -26,8 +27,17 @@ class App extends Component {
       ],
     }
   }
-  render() {
-    console.log(this);
+  getReadContent() {
+    var i = 0;
+    while(i < this.state.contents.length) {
+      var data = this.state.contents[i];
+      if(data.id === this.state.selected_content_id) {
+        return data;
+      }
+      i++;
+    }
+  }
+  getContent() {
     var _title = null;
     var _desc = null;
     var _article = null;
@@ -37,17 +47,8 @@ class App extends Component {
       _desc = this.state.welcome.desc;
       _article = <ReadContent title={_title} desc={_desc}></ReadContent>
     } else if(this.state.mode === 'read') {
-      var i = 0;
-      while(i < this.state.contents.length) {
-        var data = this.state.contents[i];
-        if(data.id === this.state.selected_content_id) {
-          _title = data.title;
-          _desc = data.desc;
-          break;
-        }
-        i++;
-      }
-      _article = <ReadContent title={_title} desc={_desc}></ReadContent>
+      var _content = this.getReadContent();
+      _article = <ReadContent title={_content.title} desc={_content.desc}></ReadContent>
     } else if(this.state.mode === 'create') {
       _article = <CreateContent onSubmit={function(_title, _desc) {
         this.max_content_id = this.max_content_id +1;
@@ -59,12 +60,34 @@ class App extends Component {
           {id: this.max_content_id, title:_title, desc:_desc}
         )
         this.setState({
-          contents: newContents
+          contents: newContents,
+          mode:'read',
+          selected_content_id:this.max_content_id
         });
         console.log(_title, _desc);
       }.bind(this)}></CreateContent>
+    } else if(this.state.mode === 'update') {
+      var _content = this.getReadContent();
+      _article = <UpdateContent data={_content} onSubmit={function(_id, _title, _desc){
+        var _contents = Array.from(this.state.contents);
+        var i = 0;
+        while(i < _contents.length) {
+          if(_contents[i].id === _id) {
+            _contents[i] = {id:_id, title:_title, desc:_desc};
+            break;
+          }
+          i++
+        }
+        this.setState({
+          contents:_contents,
+          mode:'read'
+        })
+      }.bind(this)}></UpdateContent>
     }
-
+    return _article;
+  }
+  render() {
+    console.log(this);
     return (
       <div className="App">
         <Subject 
@@ -90,7 +113,7 @@ class App extends Component {
             mode:_mode
           });
         }.bind(this)}></Control>
-        {_article}
+        {this.getContent()}
       </div>
     );
   }
